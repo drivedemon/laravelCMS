3ea1cfc2d1d059a960fa778acba390297a00b98b
drivedemon <superdriveza@hotmail.com>
2019-10-08 15:14:36 +0700

    + Add image in storage and get to view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,10 +8,27 @@
       Post
     </div>
     <div class="card-body">
-
+      <table class="table table-bordered">
+        <thead class="thead-light">
+          <th>Image</th>
+          <th>Title</th>
+          <th width="10%"></th>
+          <th width="10%"></th>
+        </thead>
+        <tbody>
+          @foreach($posts as $post)
+            <tr>
+              <td><img src="storage/{{$post->image}}" alt="" width="40px" height="40px"></td>
+              <td>{{$post->title}}</td>
+              <td><a href="" class="btn btn-info btn-sm">Edit</a></td>
+              <td><a href="" class="btn btn-danger btn-sm">Delete</a></td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
     </div>
   </div>
-  <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.4.1.js"></script> -->
   <!-- <script type="text/javascript">
     $(document).ready(function(){
       $('.delete_form').on('submit', function(){
