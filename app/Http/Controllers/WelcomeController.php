2ded2f475135eab4cf54db16922e664358cf16f8
drivedemon <superdriveza@hotmail.com>
2019-10-16 14:14:15 +0700

    + Add homepage design

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
+use App\Tag;
+use App\Post;
 
 class WelcomeController extends Controller
 {
-    //
+  public function index() {
+    return view('welcome')
+    ->with('categories', Category::all())
+    ->with('posts', Post::all())
+    ->with('tags', Tag::all());
+  }
 }
