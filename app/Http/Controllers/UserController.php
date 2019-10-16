4b6d9063b44267dcbfb38de2f9d038285ef09f99
drivedemon <superdriveza@hotmail.com>
2019-10-16 09:47:10 +0700

    + Get user data to view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
   public function index() {
-    return view('users.index');
+    // return view and query data send to view
+    return view('users.index')->with('users', User::all());
   }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,7 +5,32 @@
       User
     </div>
     <div class="card-body">
-
+      @if ($users->count() > 0)
+        <table class="table table-bordered">
+          <thead class="thead-light">
+            <th width="20%">Name</th>
+            <th>Email</th>
+            <th width="17%"></th>
+          </thead>
+          <tbody>
+            @foreach($users as $user)
+              <tr>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                @if(!$user->isAdmin())
+                <td>
+                  <button type="button" name="button" class="btn btn-primary btn-sm">Make Admin</button>
+                </td>
+                @else
+                <td></td>
+                @endif
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      @else
+        <h3 class="text text-center">No User</h3>
+      @endif
     </div>
   </div>
 @endsection
