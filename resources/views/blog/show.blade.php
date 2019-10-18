6e00cb6cb7d4af84cc9bf82ea3795ac1c6d7e9bd
drivedemon <superdriveza@hotmail.com>
2019-10-18 10:44:17 +0700

    + Add comment post by disqus

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,54 +1,69 @@
 <!DOCTYPE html>
 @extends('layouts.blog')
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
 
-      @section('title')
-        <title>
-            {{$post->title}}
-        </title>
-      @endsection
+  @section('title')
+  <title>
+    {{$post->title}}
+  </title>
+  @endsection
 
-  </head>
+</head>
 
-  <body>
-    @section('header')
-    <header class="header text-white h-fullscreen pb-80" style="background-image: url('../../storage/{{$post->image}}');" data-overlay="9">
-      <div class="container text-center">
-        <div class="row h-100">
-          <div class="col-lg-8 mx-auto align-self-center">
-            <p class="opacity-70 text-uppercase small ls-1">{{$post->category->name}}</p>
-            <h1 class="display-4 mt-7 mb-8">{{$post->title}}</h1>
-            <p><span class="opacity-70 mr-1">By</span> <a class="text-white" href="#">{{$post->user->name}}</a></p>
-            <p><img class="avatar avatar-sm" src="../assets/img/avatar/2.jpg" alt="..."></p>
-          </div>
-          <div class="col-12 align-self-end text-center">
-            <a class="scroll-down-1 scroll-down-white" href="#section-content"><span></span></a>
-          </div>
+<body>
+  @section('header')
+  <header class="header text-white h-fullscreen pb-80" style="background-image: url('../../storage/{{$post->image}}');" data-overlay="9">
+    <div class="container text-center">
+      <div class="row h-100">
+        <div class="col-lg-8 mx-auto align-self-center">
+          <p class="opacity-70 text-uppercase small ls-1">{{$post->category->name}}</p>
+          <h1 class="display-4 mt-7 mb-8">{{$post->title}}</h1>
+          <p><span class="opacity-70 mr-1">By</span> <a class="text-white" href="#">{{$post->user->name}}</a></p>
+          <p><img class="avatar avatar-sm" src="{{ Gravatar::src('$post->user->email') }}"></p>
         </div>
-      </div>
-    </header>
-    @endsection
-
-    @section('content')
-    <div class="section" id="section-content">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-              {!!$post->content!!}
-            <div class="gap-xy-2 mt-6">
-              @foreach($post->tags as $tag)
-              <a class="badge badge-pill badge-secondary" href="#">{{$tag->name}}</a>
-              @endforeach
-            </div>
-          </div>
+        <div class="col-12 align-self-end text-center">
+          <a class="scroll-down-1 scroll-down-white" href="#section-content"><span></span></a>
         </div>
       </div>
     </div>
-    @endsection
-  </body>
+  </header>
+  @endsection
+
+  @section('content')
+  <div class="section" id="section-content">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          {!!$post->content!!}
+          <div class="gap-xy-2 mt-6">
+            @foreach($post->tags as $tag)
+            <a class="badge badge-pill badge-secondary" href="#">{{$tag->name}}</a>
+            @endforeach
+          </div>
+          <br>
+          <div id="disqus_thread"></div>
+          <script>
+          var disqus_config = function () {
+            this.page.url = "{{config('app.url')}}/blog/post/{{$post->id}}";  // comment จะแสดงอยู่ในบทความนั้นๆ
+            this.page.identifier = "{{$post->id}}"; // comment ที่บ่งบอกว่าเป็น comment ของ post ไหน
+          };
+          (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://http-localhost-8000-iwgniiph4o.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+          })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+</body>
 </html>
