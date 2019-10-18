0c31093323437185cc5fecc9add2131fdb3ba4e6
drivedemon <superdriveza@hotmail.com>
2019-10-18 11:40:35 +0700

    + Add query with condtion and send data to view

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -5,10 +5,26 @@ namespace App\Http\Controllers\Blog;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Post;
+use App\Category;
+use App\Tag;
 
 class PostController extends Controller
 {
   public function show(Post $post) {
     return view('blog.show')->with('post', $post);
+  }
+  public function category(Category $category) {
+    return view('blog.category')
+    ->with('categories', Category::all())
+    ->with('tags', Tag::all())
+    ->with('category', $category)
+    ->with('posts', $category->post()->paginate(3));
+  }
+  public function tag(Tag $tag) {
+    return view('blog.tag')
+    ->with('categories', Category::all())
+    ->with('tags', Tag::all())
+    ->with('tag', $tag)
+    ->with('posts', $tag->posts()->paginate(3));
   }
 }
