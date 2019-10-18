d9323d1db97b8ee4b21d0eb8221fbd88e836fdea
drivedemon <superdriveza@hotmail.com>
2019-10-18 11:06:57 +0700

    + Add search condition and query

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,9 +10,17 @@ use App\Post;
 class WelcomeController extends Controller
 {
   public function index() {
+    // get value search by query in submit form || this is GET method
+    $search = request()->query('search');
+    // query with condition by string
+    if ($search) {
+      $post = Post::where('title', 'LIKE', "%{$search}%")->paginate(1);
+    } else {
+      $post = Post::paginate(3);
+    }
     return view('welcome')
     ->with('categories', Category::all())
-    ->with('posts', Post::paginate(4))
+    ->with('posts', $post)
     ->with('tags', Tag::all());
   }
 }
